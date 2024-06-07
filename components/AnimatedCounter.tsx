3011de3b4488c959ecb9aeec6aab90922a070e80
ahsan689 <ahsanaleem689@gmail.com>
2024-06-07 23:04:57 +0500

    CountUp component implement for account balance

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,8 +4,8 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div>
-       <CountUp 
-       duration={2.75}
+       <CountUp className="w-full"
+       duration={3}
        decimals={2}
         end={amount} 
         decimal=","
