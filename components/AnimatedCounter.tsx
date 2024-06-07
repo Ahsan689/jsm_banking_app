8da9e68c7efa37ef7d335653f2cd1acc38d32081
ahsan689 <ahsanaleem689@gmail.com>
2024-06-07 22:59:56 +0500

    implement bank details, bank account and totalCurrentBalance

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div>
+       <CountUp 
+       duration={2.75}
+       decimals={2}
+        end={amount} 
+        decimal=","
+        prefix="$"
+      />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
