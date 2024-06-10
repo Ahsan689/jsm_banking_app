4360b45e46b1cb7c7920a664694d895f3e0916cc
ahsan689 <ahsanaleem689@gmail.com>
2024-06-10 17:55:05 +0500

    sidebar CREATED

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
@@ -12,7 +13,7 @@ const Sidebar = ({user}: SiderbarProps) => {
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
             <Link href='/'
-                className='mb-12 cursor-pointer items-center gap-2'
+                className='mb-12 cursor-pointer flex items-center gap-2'
                 >
                 <Image
                     src='/icons/logo.svg'
@@ -34,12 +35,19 @@ const Sidebar = ({user}: SiderbarProps) => {
                       src={item.imgURL}
                       alt={item.label}
                       fill
+                      className={cn({'brightness-[3] invert-0':isActive})}
                       />
                   </div>
+                  <p className={cn('sidebar-label',{'!text-white':isActive})}>
+                    {item.label}
+                  </p>
                 </Link>
               )
             })}
+
+            USER
         </nav>
+        FOOTER
     </section>
   )
 }
